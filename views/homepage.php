a92994c9daaa891fb49e53799d0c9bfecfb7367d
Unknowncall <zharvey3131@gmail.com>
2019-10-31 11:50:44 -0500

    Worked on implementing the homepage

--- views/homepage.php
+++ views/homepage.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<div class="row">
-		<div class="column">
+		<div class="column top_airports">
 			<table class="table table-striped">
 				<tr>
 					<td>Name</td>
@@ -12,16 +12,31 @@
 						$city = $x['city'];
 						$country = $x['country'];
 						echo "<tr>";
-					echo "<td> {$name} </td>";
-					echo "<td> {$city}, {$country} </td>";
-					echo "</tr>";
+							echo "<td> {$name} </td>";
+							echo "<td> {$city}, {$country} </td>";
+						echo "</tr>";
 					}
 				?>
 			</table>
 		</div>
-		<div class="column">
+		<div class="column paragraph" style="float:left">
 			<center><h1>Airport Database</h1></center>
-			<!--<p>Welcome to our Database Managment Systems project. This was developed by 3 students at Whitewater Wisconsin, Zach Harvey, Ryan {LASTNAME}, and Jake {LASTNAME}. This project was made strictly for education purposes. Our data was collected from <a href="http://www.openflights.org/" target="_blank">openflights.org</a>.</p>-->
+			<p>Welcome to our Database Managment Systems project. This was developed by 3 students at Whitewater Wisconsin, Zach Harvey, Ryan {LASTNAME}, and Jake {LASTNAME}. This project was made strictly for education purposes. Our data was collected from <a href="http://www.openflights.org/" target="_blank">openflights.org</a>.</p>
 		</div>
 	</div>
 </div>
+
+<style>
+.top_airports{
+    width:55%;
+    margin-right:2%;
+    float: left;
+}
+.top_airports table{
+   width:100%;
+}
+.paragraph {
+    width:42%;
+    float: left;
+}
+</style>
