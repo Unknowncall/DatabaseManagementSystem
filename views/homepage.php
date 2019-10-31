4f3cd8d18e28eeeeb7c9ccaa70f298be7b09414f
Unknowncall <zharvey3131@gmail.com>
2019-10-31 12:16:59 -0500

    Worked on implementing the homepage

--- views/homepage.php
+++ views/homepage.php
@@ -1,42 +1,39 @@
 <div class="container">
 	<div class="row">
-		<div class="column top_airports">
-			<table class="table table-striped">
-				<tr>
-					<td>Name</td>
-					<td>City, Country</td>
-				</tr>
-				<?php
-					foreach ($dataList as $x) {
-						$name = $x['name'];
-						$city = $x['city'];
-						$country = $x['country'];
-						echo "<tr>";
-							echo "<td> {$name} </td>";
-							echo "<td> {$city}, {$country} </td>";
-						echo "</tr>";
-					}
-				?>
-			</table>
-		</div>
 		<div class="column paragraph" style="float:left">
 			<center><h1>Airport Database</h1></center>
 			<p>Welcome to our Database Managment Systems project. This was developed by 3 students at Whitewater Wisconsin, Zach Harvey, Ryan {LASTNAME}, and Jake {LASTNAME}. This project was made strictly for education purposes. Our data was collected from <a href="http://www.openflights.org/" target="_blank">openflights.org</a>.</p>
 		</div>
+		<div class="column top_airports">
+			<h1>Want to learn about a random airport?</h1>
+			<?php
+
+				$sql = "SELECT COUNT(*) FROM `airports`";
+
+				$data = getOneRecord($sql, $db, null);
+
+				foreach ($data as $x){
+					$airportSelection = rand(0, $x);
+
+					$sql2 = "SELECT * FROM `airports` WHERE airport_id = {$airportSelection}";
+					
+					$data2 = getOneRecord($sql2, $db, null);
+					
+				}
+
+			?>
+		</div>
 	</div>
 </div>
-
 <style>
 .top_airports{
-    width:55%;
-    margin-right:2%;
-    float: left;
+width:40%;
+margin-right:2%;
+float: left;
 }
-.top_airports table{
-   width:100%;
-}
+
 .paragraph {
-    width:42%;
-    float: left;
+width:60%;
+float: left;
 }
 </style>
