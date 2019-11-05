754fa180bdce6547307df6fadb698f9ecfec0069
Unknowncall <zharvey3131@gmail.com>
2019-11-04 19:06:59 -0600

    Started the airport review screen

--- views/airportreview.php
+++ views/airportreview.php
@@ -1,4 +1,20 @@
 <div class="container">
-    <center><h1>You are about to leave a review about the airport:</h1>
+    <center><h2>You are about to leave a review about the airport:</h2>
     <h3>{$name}</h3></center>
+
+    <style>
+        .radio-input {
+            display: inline-flex;
+            justify-content: space-around;
+            vertical-align: top;
+        }
+    </style>
+
+    <form>
+        <input class='radio-input' type="radio" name="rating" value="1" checked> 1
+        <input class='radio-input' type="radio" name="rating" value="2"> 2
+        <input class='radio-input' type="radio" name="rating" value="3"> 3
+        <input class='radio-input' type="radio" name="rating" value="2"> 4
+        <input class='radio-input' type="radio" name="rating" value="3"> 5</br>
+    </form>
 </div>
